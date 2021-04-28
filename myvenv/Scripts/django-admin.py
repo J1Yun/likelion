6f9a21b4713bcf1e157dc08b886a9a9a87a9deff
J1yun <cjy5025@naver.com>
2021-04-28 20:47:37 +0900

    word counter

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,21 @@
+#!c:\users\jiyun\desktop\최지윤\멋쟁이사자처럼\정규 세션\3차시\과제\myvenv\scripts\python.exe
+# When the django-admin.py deprecation ends, remove this script.
+import warnings
+
+from django.core import management
+
+try:
+    from django.utils.deprecation import RemovedInDjango40Warning
+except ImportError:
+    raise ImportError(
+        'django-admin.py was deprecated in Django 3.1 and removed in Django '
+        '4.0. Please manually remove this script from your virtual environment '
+        'and use django-admin instead.'
+    )
+
+if __name__ == "__main__":
+    warnings.warn(
+        'django-admin.py is deprecated in favor of django-admin.',
+        RemovedInDjango40Warning,
+    )
+    management.execute_from_command_line()
